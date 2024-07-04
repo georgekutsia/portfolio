610e6674a3ab410e1d00cff5e60d6a278d71139c
George <georgekutsia@gmail.com>
2024-07-04 10:12:11 +0100

    profile sizing

--- src/components/profileComponent/ProfileComponent.jsx
+++ src/components/profileComponent/ProfileComponent.jsx
@@ -1,9 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { GlobalContext } from '../../App';
-import { MyPictureComponent, ProjectComponent,  } from '../index'
+import { MyPictureComponent, ProjectComponent } from '../index';
 
 function ProfileComponent() {
   const { darkLightText, language, normalCartoon } = useContext(GlobalContext);
+  const projectsRef = useRef(null);
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+      if (window.scrollY >= 0 && window.scrollY <= 149) {
+        projectsRef.current.style.marginLeft = "0%";
+
+      } else if (window.scrollY >= 150 && window.scrollY <= 299) {
+        projectsRef.current.style.marginLeft = "30%";
+      } else if (window.scrollY >= 300 && window.scrollY <= 449) {
+
+      } else if (window.scrollY >= 450) {
+        if (projectsRef.current) {
+          // pictureRef.current.style.marginLeft = "0px";
+        }
+
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
+
 
   const profPic1 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1718972201/portfolio/Profile%20pictures/Img_Perfil1_ianbeb.png";
   const profPic2 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1718972201/portfolio/Profile%20pictures/Img_Perfil4_kmw3w0.png";
@@ -14,18 +43,22 @@ function ProfileComponent() {
   const profDraw2 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1719569571/img2_fofxdi.png";
   const profDraw3 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1719569571/img4_o51prl.png";
   const profDraw4 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1719569575/img3_iqk9oj.png";
-  const icons1 = [ 'fa-brands fa-react', 'fa-brands fa-html5', 'fa-brands fa-sass', 'fa-brands fa-css3','fa-brands fa-angular','fa-brands fa-bootstrap',];
-  const icons2 = [ 'fa-brands fa-canvas', 'icon2', 'icon3', 'icon4', 'icon5', 'icon6', 'icon7'];
-  const img = [""]
+
+  const projectImg1= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905077/proyecto%20final/usuarios/vamos-a-arreglarlo-retrato-de-un-joven-y-guapo-hombre-de-mano-que-esta-de-pie-frente-a-sus-herramientas-de-trabajo-2k2c58g_vtfaqg.jpg";
+  const projectImg2= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905079/proyecto%20final/usuarios/IMG_1192-2_tcl82c.webp";
+
+  const programsUsed1 = ["img/prExpres.png", "img/prAngular.png"];
+  const programsUsed2 = ["img/prJs.png", "img/prHtml.png", "img/prCss.png", "img/prHtml.png"];
+
   return (
     <div>
-    <section className='projects'>
-      <ProjectComponent icons={icons1}/>
-      <ProjectComponent icons={icons2}/>
-    </section>
-    {normalCartoon && <MyPictureComponent profPic1={profPic1} profPic2={profPic2} profPic3={profPic3} profPic4={profPic4} saturate={2.4} /> }
-    {!normalCartoon &&   <MyPictureComponent profPic1={profDraw1} profPic2={profDraw2} profPic3={profDraw3} profPic4={profDraw4} saturate={1}/>}
-        <h3>fldaf</h3>
+      <section ref={projectsRef} className='projects'>
+        <ProjectComponent nombreProyecto={"Bulala"} projectImg={projectImg1}  programsUsed={programsUsed1} />
+        <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg2}  programsUsed={programsUsed2} />
+      </section>
+      {normalCartoon && <MyPictureComponent profPic1={profPic1} profPic2={profPic2} profPic3={profPic3} profPic4={profPic4} saturate={2.4} />}
+      {!normalCartoon && <MyPictureComponent profPic1={profDraw1} profPic2={profDraw2} profPic3={profDraw3} profPic4={profDraw4} saturate={1} />}
+      <h3>fldaf</h3>
     </div>
   );
 }
--- src/components/profileComponent/projects/ProjectComponent.jsx
+++ src/components/profileComponent/projects/ProjectComponent.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-function ProjectComponent({icons}) {
+function ProjectComponent({nombreProyecto, programsUsed, projectImg }) {
   return (
     <div className='project'>
-        <header className='project-header'>Hola</header>
-        <img  className='project-img' src="https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905079/proyecto%20final/usuarios/IMG_1192-2_tcl82c.webp" alt="project" />
-        <footer className='project-footer'>
-          {icons.map((icon, index) => (
-            <i key={index} className={`project-footer-i ${icon}`}></i>
-          ))}
-        </footer>
+      <header className='project-header'>{nombreProyecto}</header>
+      <img className='project-img' src={projectImg} alt="project" />
+      <footer className='project-footer'>
+        {programsUsed.map((image, index) => (
+          <img key={index} src={image} alt="img" className='project-footer-img'/>
+        ))}
+      </footer>
     </div>
-  )
+  );
 }
 
-export default ProjectComponent
+export default ProjectComponent;
