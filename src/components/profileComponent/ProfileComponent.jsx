6591cf34efdbc851ff02329001637c7da2a30797
George <georgekutsia@gmail.com>
2024-07-04 10:32:46 +0100

    ajustando tamaños de project

--- src/components/profileComponent/ProfileComponent.jsx
+++ src/components/profileComponent/ProfileComponent.jsx
@@ -44,8 +44,9 @@ function ProfileComponent() {
   const profDraw3 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1719569571/img4_o51prl.png";
   const profDraw4 = "https://res.cloudinary.com/dxnzcewsy/image/upload/v1719569575/img3_iqk9oj.png";
 
-  const projectImg1= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905077/proyecto%20final/usuarios/vamos-a-arreglarlo-retrato-de-un-joven-y-guapo-hombre-de-mano-que-esta-de-pie-frente-a-sus-herramientas-de-trabajo-2k2c58g_vtfaqg.jpg";
+  const projectImg1= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905096/proyecto%20final/usuarios/trabajador-joven-usa-movil-oficina-2718441_uech8z.webp";
   const projectImg2= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905079/proyecto%20final/usuarios/IMG_1192-2_tcl82c.webp";
+  const projectImg3= "https://res.cloudinary.com/dxnzcewsy/image/upload/v1693905092/proyecto%20final/usuarios/portada-foto-perfil-redes-sociales-consejos_t022ap.jpg";
 
   const programsUsed1 = ["img/prExpres.png", "img/prAngular.png"];
   const programsUsed2 = ["img/prJs.png", "img/prHtml.png", "img/prCss.png", "img/prHtml.png"];
@@ -55,9 +56,7 @@ function ProfileComponent() {
       <section ref={projectsRef} className='projects'>
         <ProjectComponent nombreProyecto={"Bulala"} projectImg={projectImg1}  programsUsed={programsUsed1} />
         <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg2}  programsUsed={programsUsed2} />
-        <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg2}  programsUsed={programsUsed2} />
-        <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg2}  programsUsed={programsUsed2} />
-        <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg2}  programsUsed={programsUsed2} />
+        <ProjectComponent nombreProyecto={"Cacafuti"} projectImg={projectImg3}  programsUsed={programsUsed2} />
       </section>
       {normalCartoon && <MyPictureComponent profPic1={profPic1} profPic2={profPic2} profPic3={profPic3} profPic4={profPic4} saturate={2.4} />}
       {!normalCartoon && <MyPictureComponent profPic1={profDraw1} profPic2={profDraw2} profPic3={profDraw3} profPic4={profDraw4} saturate={1} />}
